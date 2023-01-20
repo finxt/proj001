048c5db1f530cd41530f94274535dce29fb204f6
finxt <okita@inext-co.com>
2023-01-21 02:28:36 +0900

    Dockerfile4

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "00-laravel-deploy.sh"
+echo "00-laravel-deploy.sh"
+echo "00-laravel-deploy.sh"
+
+#docker run --rm \
+#    -u "$(id -u):$(id -g)" \
+#    -v "$(pwd):/var/www/html" \
+#    -w /var/www/html \
+#    laravelsail/php81-composer:latest \
+#    composer install --ignore-platform-reqs
+
+
